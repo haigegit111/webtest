cf2152f5a87b347b8fb672e27722213682f327db
mike <mike@007vin.com>
2021-01-27 19:31:07 +0800

    add1

--- 06-reactapp/src/pages/Status/Status.jsx
+++ 06-reactapp/src/pages/Status/Status.jsx
@@ -10,7 +10,26 @@ const scaleNames = {
   c: 'Celsius',
   f: 'Fahrenheit'
 }
-class Calculator extends Component {
+//转化为C
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9
+}
+//转化为F
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32
+}
+//检查格式
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return ''
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+//输入组件
+class TemperatureInput extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -18,27 +37,66 @@ class Calculator extends Component {
     }
   }
   handleChange = (e) => {
-    this.setState({
-      temperature: e.target.value
-    })
-    return
+    this.props.onTemperatureChange(e.target.value)
   }
   render() {
-    const { temperature } = this.state
+    const { scale, temperature } = this.props;
     return (
       <Fragment>
         <fieldset>
-          <legend>Enter temperature in Celsius:</legend>
+          <legend>Enter temperature in {scaleNames[scale]}:</legend>
           <input
             value={temperature}
             onChange={this.handleChange}
           />
-          <BoilingVerdict celsius={parseFloat(temperature)} />
         </fieldset>
       </Fragment>
     )
   }
 }
+//主组件
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: '',
+      scale: 'c'
+    }
+  }
+  handleCelsiusChange = (temperature) => {
+    this.setState({
+      scale: 'c',
+      temperature
+    })
+  }
+  handleFahrenheitChange = (temperature) => {
+    this.setState({
+      scale: 'f',
+      temperature
+    })
+  }
+  render() {
+    const { scale, temperature } = this.state;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+    return (
+      <Fragment>
+        <TemperatureInput
+          scale='c'
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale='f'
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celsius={parseFloat(celsius)}/>
+      </Fragment>
+    )
+  }
+}
+
 class Status extends Component {
   constructor(props) {
     super(props)
@@ -49,7 +107,7 @@ class Status extends Component {
   render() {
     return (
       <Fragment>
-        <Calculator />
+        <Calculator/>
       </Fragment>
     )
   }
